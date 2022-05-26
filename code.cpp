6be5d3ea17c389e261ec398692d4daad812c6d54
aoaoaoaoaoaoaoi <40077710+aoaoaoaoaoaoaoi@users.noreply.github.com>
2022-05-27 02:15:35 +0900

    split method

--- code.cpp
+++ code.cpp
@@ -3,6 +3,31 @@
 using namespace std;
 #include <vector>
 
+/**
+ * ヘッダーの読み込み
+ * 上から3行 
+ */
+void getHeader(fstream &file, int &width, int &height){
+    string line;
+
+    //P6であることを確認
+    getline(file, line);
+    if(line != "P6"){
+        cout << "Different types of files" << endl;
+        return;
+    }
+
+    //高さ、横幅（空白区切り）
+    getline(file, line, ' ');
+    width = stoi(line);
+    getline(file, line);
+    height = stoi(line);
+    
+    //彩度の最大値
+    getline(file, line);
+    int maxChroma = stoi(line); 
+}
+
 int main(){
     string READ_FILE = "./test.ppm";
 
@@ -11,26 +36,10 @@ int main(){
         return EXIT_FAILURE;
     }
 
-    //ヘッダーの読み込み
-    //上から3行はイメージの情報なので上限なしに読み込んでも問題ない
-    string line;
-
-    //P6であることを確認
-    getline(file, line);
-    if(line != "P6"){
-        cout << "Different types of files" << endl;
-    }
-    //高さ、横幅（空白区切り）
-    getline(file, line, ' ');
-    int width = stoi(line);
-    getline(file, line);
-    int height = stoi(line);
-    //彩度の最大値
-    getline(file, line);
-    int maxChroma = stoi(line);   
+    int width, height;
+    getHeader(file, width, height);   
     
 
-    cout << line << endl;
 
     file.close();
     return 0;    
