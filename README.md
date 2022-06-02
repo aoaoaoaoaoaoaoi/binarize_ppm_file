1faea138b48abe84657001a19e4dbe5401f94f17
aoaoaoaoaoaoaoi <40077710+aoaoaoaoaoaoaoi@users.noreply.github.com>
2022-06-02 23:42:43 +0900

    add explain

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# count_the_number_of_items
+# ppmファイルの二値化
+## 設定値
+以下部分に直接記載<br>
+
+code.cppの64-71行目
+```c++
+    //読み込みファイル
+    const string READ_FILE = "./test.ppm";
+    const int MAX_WIDTH = 600;
+    const int MAX_HEIGHT = 600;
+    //出力ファイル
+    const string WRITE_FILE = "./out_test.ppm";
+    //黒と白の区切り
+    const int VALUE = 205;
+```
+
+|  定数  |  内容  |
+| ---- | ---- |
+|  READ_FILE  |  変換前のppmファイル  |
+|  MAX_WIDTH  |  変換前のファイルの最大の横幅  |
+|  MAX_HEIGHT  |  変換前のファイルの最大の縦幅  |
+|  WRITE_FILE  |  変換した出力ppmファイル  |
+|  VALUE  |  白と黒の区切り（RGB平均値）  |
+
+## 内容
+ファイルの各色のRGBの平均値を算出し、```VALUE```で区切って二値化する
+
--- code.cpp
+++ code.cpp
@@ -37,33 +37,10 @@ void writeHeader(fstream &file, int width, int height, int maxChroma){
     file << maxChroma << endl;
 }
 
-int main(){
-    const string READ_FILE = "./test.ppm";
-    const string WRITE_FILE = "./out_test.ppm";
-
-    //参照するファイル
-    fstream readFile(READ_FILE, ios::in|ios::binary);
-    if(!readFile.is_open()){
-        return EXIT_FAILURE;
-    }
-    //ヘッダー読み込み
-    int width, height, maxChroma = 0;
-    getHeader(readFile, width, height, maxChroma);   
-    if(width == 0 || height == 0){
-        return EXIT_FAILURE;
-    }
-
-    //ファイル書き込み
-    fstream beforeWriteFile(WRITE_FILE);
-    if(beforeWriteFile.is_open()){
-        //ファイル削除
-        remove(WRITE_FILE.c_str());
-    }
-    fstream writeFile;
-    writeFile.open(WRITE_FILE, ios::out|ios::binary);
-    writeHeader(writeFile, width, height, maxChroma);
-
-    //内容
+/**
+ * 二値化した値を書き込む
+ */ 
+void binalize(fstream &readFile, fstream &writeFile, int value){
     unsigned char p;
     const int RGB_COUNT = 3;
     //黒と白の区切り
@@ -73,7 +50,7 @@ int main(){
     while(readFile.read((char*)&p, sizeof p)){
         sum += (int)p;
         if(++c % RGB_COUNT == 0){
-            unsigned char wr = (VALUE < (sum / RGB_COUNT)) ? 255 : 0;
+            unsigned char wr = (value < (sum / RGB_COUNT)) ? 255 : 0;
             sum = 0;
             c = 0;
             for(int i=0; i < RGB_COUNT; ++i){
@@ -81,6 +58,41 @@ int main(){
             }
         }
     }
+}
+
+int main(){
+    //読み込みファイル
+    const string READ_FILE = "./test.ppm";
+    const int MAX_WIDTH = 600;
+    const int MAX_HEIGHT = 600;
+    //出力ファイル
+    const string WRITE_FILE = "./out_test.ppm";
+    //黒と白の区切り
+    const int VALUE = 205;
+
+    //読み込みファイル
+    fstream readFile(READ_FILE, ios::in|ios::binary);
+    if(!readFile.is_open()){
+        return EXIT_FAILURE;
+    }
+    int width, height, maxChroma = 0;
+    getHeader(readFile, width, height, maxChroma);   
+    if(width == 0 || height == 0 || MAX_WIDTH < width || MAX_HEIGHT < height){
+        return EXIT_FAILURE;
+    }
+
+    //書き込みファイル
+    fstream beforeWriteFile(WRITE_FILE);
+    if(beforeWriteFile.is_open()){
+        //ファイル削除
+        remove(WRITE_FILE.c_str());
+    }
+    fstream writeFile;
+    writeFile.open(WRITE_FILE, ios::out|ios::binary);
+    writeHeader(writeFile, width, height, maxChroma);
+
+    //二値化
+    binalize(readFile, writeFile, VALUE);
 
     writeFile.close();
     readFile.close();
